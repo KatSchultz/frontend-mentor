1cd98c37d755d2e8efaa270000f6cf11aa0405a2
KatSchultz <ks.katieschultz@gmail.com>
2022-11-07 10:56:19 -0500

    start js targeting

---
+++ interactive-card-details-form-main/script.js
@@ -0,0 +1,5 @@
+const nameDisplay = documet.getElementById("card-name-display");
+const numberDisplay = documet.getElementById("card-num-display");
+const monthDisplay = documet.getElementById("mon-display");
+const yearDisplay = documet.getElementById("year-display");
+const cvcDisplay = documet.getElementById("cvc-display");
