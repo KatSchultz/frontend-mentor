be4e64986a9d4cfe5c825cf6a1e5282e311b29fb
KatSchultz <ks.katieschultz@gmail.com>
2022-11-07 14:21:39 -0500

    started adding event listeners

--- interactive-card-details-form-main/script.js
+++ interactive-card-details-form-main/script.js
@@ -1,5 +1,30 @@
-const nameDisplay = documet.getElementById("card-name-display");
-const numberDisplay = documet.getElementById("card-num-display");
-const monthDisplay = documet.getElementById("mon-display");
-const yearDisplay = documet.getElementById("year-display");
-const cvcDisplay = documet.getElementById("cvc-display");
+const nameDisplay = document.getElementById("card-name-display");
+const numberDisplay = document.getElementById("card-num-display");
+const monthDisplay = document.getElementById("mon-display");
+const yearDisplay = document.getElementById("year-display");
+const cvcDisplay = document.getElementById("cvc-display");
+
+const nameInput = document.getElementById("name-input");
+const numberInput = document.getElementById("number-input");
+const monthInput = document.getElementById("month-input");
+const yearInput = document.getElementById("year-input");
+const cvcInput = document.getElementById("cvc-input");
+
+nameDisplay.innerText = nameInput.innerText;
+
+nameInput.addEventListener("keyup", () => {
+  nameDisplay.innerText = nameInput.value;
+});
+
+numberInput.addEventListener("keyup", () => {
+  numberDisplay.innerText = numberInput.value;
+});
+monthInput.addEventListener("keyup", () => {
+  monthDisplay.innerText = monthInput.value;
+});
+yearInput.addEventListener("keyup", () => {
+  yearDisplay.innerText = yearInput.value;
+});
+cvcInput.addEventListener("keyup", () => {
+  cvcDisplay.innerText = cvcInput.value;
+});
