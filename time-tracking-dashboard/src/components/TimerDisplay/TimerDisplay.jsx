79ebec92f4e155fdc9edd2d3b6902d055810aeef
KatSchultz <ks.katieschultz@gmail.com>
2022-11-10 16:26:17 -0500

    added icons

--- time-tracking-dashboard/src/components/TimerDisplay/TimerDisplay.jsx
+++ time-tracking-dashboard/src/components/TimerDisplay/TimerDisplay.jsx
@@ -1,47 +1,54 @@
 import React from "react";
 import "./TimerDisplay.css";
+import iconplay from "../../images/icon-play.svg";
+import iconExercise from "../../images/icon-exercise.svg";
+import iconSelfCare from "../../images/icon-self-care.svg";
+import iconSocial from "../../images/icon-social.svg";
+import iconStudy from "../../images/icon-study.svg";
+import iconWork from "../../images/icon-work.svg";
+import iconElips from "../../images/icon-ellipsis.svg";
 
 export default function TimerDisplay(props) {
   const propTitle = props.timer.title;
 
-  let titleStyles;
-  let color;
+  let color, icon;
 
   if (propTitle === "Work") {
     color = "hsl(15, 100%, 70%)";
+    icon = iconWork;
   }
   if (propTitle === "Play") {
     color = "hsl(195, 74%, 62%)";
+    icon = iconplay;
   }
   if (propTitle === "Study") {
     color = "hsl(348, 100%, 68%)";
+    icon = iconStudy;
   }
   if (propTitle === "Exercise") {
     color = "hsl(145, 58%, 55%)";
+    icon = iconExercise;
   }
   if (propTitle === "Social") {
     color = "hsl(264, 64%, 52%)";
+    icon = iconSocial;
   }
   if (propTitle === "Self Care") {
     color = "hsl(43, 84%, 65%)";
+    icon = iconSelfCare;
   }
 
-  if (propTitle === "Work") {
-    titleStyles = {
-      backgroundColor: "hsl(15, 100%, 70%)",
-    };
-  }
-
-  titleStyles = {
+  const titleStyles = {
     backgroundColor: color,
   };
 
   return (
     <div className="timer-container" style={titleStyles}>
+      <img src={icon} alt="" className="icon" />
       <div className="timer-info">
         <div className="title">
           <h3>{props.timer.title}</h3>
-          <p className="timeP">...</p>
+          <img src={iconElips} alt="" className="ellipsis" />
         </div>
         <div className="time">
           {props.timePeriod === "daily" && (
