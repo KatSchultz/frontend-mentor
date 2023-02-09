91c1ec4487361be16b8fe81c918280672ebff014
KatSchultz <ks.katieschultz@gmail.com>
2023-02-09 14:12:40 -0500

    added additional stories section

--- news-homepage/src/components/AdditionalStories.jsx
+++ news-homepage/src/components/AdditionalStories.jsx
@@ -1,5 +1,54 @@
 import React from "react";
+import "./AdditionalStories.css";
+import pic1 from "../images/image-retro-pcs.jpg";
+import pic2 from "../images/image-top-laptops.jpg";
+import pic3 from "../images/image-gaming-growth.jpg";
 
 export default function AdditionalStories() {
-  return <div>AdditionalStories</div>;
+  return (
+    <div>
+      <div className="stories story-1">
+        <div className="story-img">
+          <picture>
+            <img src={pic1} alt="" />
+          </picture>
+        </div>
+        <div className="story-content">
+          <h2>01</h2>
+          <h3 className="story-title">Reviving Retro PCs</h3>
+          <p className="story-text">
+            What happens when old PCs are given modern upgrades?
+          </p>
+        </div>
+      </div>
+      <div className="stories story-1">
+        <div className="story-img">
+          <picture>
+            <img src={pic2} alt="" />
+          </picture>
+        </div>
+        <div className="story-content">
+          <h2>02</h2>
+          <h3 className="story-title">Top 10 Laptops of 2022</h3>
+          <p className="story-text">
+            Our best pics for various needs and budgets.
+          </p>
+        </div>
+      </div>
+      <div className="stories story-1">
+        <div className="story-img">
+          <picture>
+            <img src={pic3} alt="" />
+          </picture>
+        </div>
+        <div className="story-content">
+          <h2>03</h2>
+          <h3 className="story-title">The Growth of Gaming</h3>
+          <p className="story-text">
+            How the pandemic has sparked fresh opportunities.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- news-homepage/src/components/New.jsx
+++ news-homepage/src/components/New.jsx
@@ -1,5 +1,30 @@
 import React from "react";
+import "./New.css";
 
 export default function New() {
-  return <div>New</div>;
+  return (
+    <div className="news-section">
+      <h3 className="new-title">New</h3>
+      <div className="story story-border">
+        <h4>Hydrogen VS Electric Cars</h4>
+        <p className="story-text">
+          Will hydrogen-fueled cars ever catch up to EVs?
+        </p>
+      </div>
+      <div className="story story-border">
+        <h4>The Downsides of AI Artistry</h4>
+        <p className="story-text">
+          What are the possible adverse effects of on-demand AI image
+          generation?
+        </p>
+      </div>
+      <div className="story">
+        <h4>Is VC Funding Drying Up?</h4>
+        <p className="story-text">
+          Private funding by VC firms is down 50% YOY. We take a look at what
+          that means.
+        </p>
+      </div>
+    </div>
+  );
 }
